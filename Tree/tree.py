1955f9dd7eb518674025d08676430847307c86c0
Md-Meadul-Islam <mdmeadulislam@gmail.com>
2026-02-09 22:41:50 +0600

    tree

--- Tree/tree.py
+++ Tree/tree.py
@@ -5,22 +5,3 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-
-    def serialize(self, root):
-        if not root:
-            return []
-
-        result = []
-        queue = deque([root])
-
-        while queue:
-            node = queue.popleft()
-
-            if node:
-                result.append(node.val)
-                queue.append(node.left)
-                queue.append(node.right)
-            else:
-                result.append(None)
-
-        return result
